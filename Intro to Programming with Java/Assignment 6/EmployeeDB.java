025cd4c82ce9d09ad825560c6bf0c88ed42ba5ca
Sean Connor <sc.higher@gmail.com>
2018-03-09 00:40:40 +0000

    add assignment 6

---
+++ Intro to Programming with Java/Assignment 6/EmployeeDB.java
@@ -0,0 +1,11 @@
+/**
+ * This program is my response to Assignment 5 for
+ * the class 605.201.81 Intro to Programming Using
+ * Java at the JHU EPP CS program.
+ *
+ * This is a morse code translator program. It will
+ * translate text to morse code and vice-versa.
+ *
+ * @author: Sean Connor
+ */
+
