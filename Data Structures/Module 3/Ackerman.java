7eb8b5fadea819277ede7c8dbb9145a6c800ea5d
Sean Connor <sc.higher@gmail.com>
2018-06-20 00:03:02 -0400

    update hws

---
+++ Data Structures/Module 3/Ackerman.java
@@ -0,0 +1,23 @@
+public class Ackerman
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Ackerman(2,2) = " + ack(2,2));
+    }
+
+    public static int ack(int m, int n)
+    {
+        if( m == 0 )
+        {
+            return n + 1;
+        }
+        else if( n == 0 )
+        {
+            return ack(m-1, 1);
+        }
+        else
+        {
+            return ack(m-1, ack(m, n-1));
+        }
+    }
+}
